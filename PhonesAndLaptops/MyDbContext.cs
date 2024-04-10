b70b0785563bda0a37e7e45786cdc40419dc72a0
olegmakeienko <molva83@gmail.com>
2024-04-10 14:26:23 +0200

    oneModelCreating void added

--- PhonesAndLaptops/MyDbContext.cs
+++ PhonesAndLaptops/MyDbContext.cs
@@ -7,6 +7,17 @@ public class MyDbContext : DbContext
     public DbSet<Laptop> Laptops { get; set; }
     public DbSet<MobilePhone> MobilePhones { get; set; }
     public DbSet<Office> Offices { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Definiera relationen mellan Office och Assets
+        modelBuilder.Entity<Asset>()
+            .HasOne(a => a.Office) // Varje Asset har ett Office
+            .WithMany(o => o.Assets) // Ett Office har många Assets
+            .HasForeignKey(a => a.OfficeId); // ForeignKey i Asset som pekar på Office
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
